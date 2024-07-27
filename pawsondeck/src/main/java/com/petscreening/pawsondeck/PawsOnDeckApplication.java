34e22dea292a4187bcbee47d11e370f7225d7cf2
Hayden Key <hwkey.77@gmail.com>
2024-07-27 01:14:46 -0400

    initialize Spring project

---
+++ pawsondeck/src/main/java/com/petscreening/pawsondeck/PawsOnDeckApplication.java
@@ -0,0 +1,13 @@
+package com.petscreening.pawsondeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsOnDeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawsOnDeckApplication.class, args);
+	}
+
+}
---
+++ pawsondeck/src/test/java/com/petscreening/pawsondeck/PawsOnDeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petscreening.pawsondeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsOnDeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
